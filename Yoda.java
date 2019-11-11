3fcf7e87d7c12735f10d29c2bbb8e69f792a313d
tocsika7 <43522870+tocsika7@users.noreply.github.com>
2019-11-11 20:39:06 +0100

    Add files via upload

---
+++ Yoda.java
@@ -0,0 +1,10 @@
+public class Yoda{
+	public static void main(String[] args) {
+		String myString = null;
+		//if(myString.equals("foobar")){}//Nem Yoda
+		
+
+		if("foobar".equals(myString)){}
+System.out.println("Nincs hibaüzenet"); //Yoda
+	}
+}
